eee95646539d06f21ce532e7e785f0faceffebc4
abdur-git <abdur09266@gmail.com>
2025-07-01 23:48:18 +0600

    allow origin

--- api/create.php
+++ api/create.php
@@ -1,4 +1,9 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
     header('Content-Type: application/json');
     require_once('dbcon.php');
     $user_id = 0;
